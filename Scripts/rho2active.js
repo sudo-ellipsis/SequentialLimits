7ad36edb8fbe611cb58ca8c6c695132e5b8812d6
ellipsis <ellipsis.threedots@gmail.com>
2022-01-14 16:31:56 +0800

    reworked script to be more like buyall, with auto milestone and more csv-friendly but less human-readable output

--- Scripts/rho2active.js
+++ Scripts/rho2active.js
@@ -1,19 +1,21 @@
-let targetMultiplier = 2
+let targetMultiplier = 5
+
 
 function getTimeString(seconds) {
   let d = Math.floor(seconds / (3600*24));
   let h = Math.floor(seconds % (3600*24) / 3600);
   let m = Math.floor(seconds % 3600 / 60);
   let s = Math.floor(seconds % 60);
-  return d + "d " +
-         h.toString().padStart(2,'0') + "h" +
-         m.toString().padStart(2,'0') + "m" +
-         s.toString().padStart(2,'0') + "s"
+  return "," + d + "," +
+         h.toString().padStart(2,'0') + ":" +
+         m.toString().padStart(2,'0') + ":" +
+         s.toString().padStart(2,'0') + ","
 }
 
 remote("let hasPublished = false")
 let tickLength = 1
 let totalTime = 0
+let tauAtPub = 0
 
 while (true) {
   let currency = remote(
@@ -21,6 +23,10 @@ while (true) {
     "game.buy(theory.upgrades)\n" +
     "game.buy(theory.permanentUpgrades)\n" +
     "hasPublished = false\n" +
+    "if(theory.milestonesUnused > 0){\n" +
+    "let m = [1,1,1,4,4,4,4,3,3,2,2,2,2,2]\n" +
+    "theory.milestoneUpgrades[m[theory.milestonesTotal-1]-1].buy(1)}\n" +
+
     "if (theory.isPublicationAvailable) {\n" +
     "  let m = getPublicationMultiplier(theory.tau)/\n" +
     "          getPublicationMultiplier(theory.tauPublished.max(1))\n" +
@@ -31,21 +37,21 @@ while (true) {
     "}\n" +
     "currency.value")
   if (remote("hasPublished") == "true") {
+    log(getTimeString(totalTime) + tauAtPub)
     tickLength = 1
-    for (let i = 0 ; i < 60; ++i) {
+    for (let i = 0 ; i < 120; ++i) {
         remote("theory.upgrades[0].buy(-1)\n" +
                "theory.upgrades[1].buy(-1)\n" +
                "theory.tick(1, 1)")
     }
-    log("Published")
   }
   if (remote("theory.milestonesUnused > 0") == "true") {
-    log("Please select a milestone in the game UI.")
     while (remote("theory.milestonesUnused > 0") == "true") {
       System.Threading.Thread.Sleep(500)
     }
+   
   }
+  tauAtPub = currency
   totalTime += tickLength
-  tickLength = Math.min(60, tickLength*1.05)
-  log(getTimeString(totalTime) + " | " + currency)
+  tickLength = Math.min(180, tickLength*1.2)
 }
