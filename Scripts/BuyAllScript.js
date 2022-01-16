de4c9b549758da1490da37151b60e81a52f32baf
ellipsis <ellipsis.threedots@gmail.com>
2022-01-16 16:55:20 +0800

    rebalance

--- Scripts/BuyAllScript.js
+++ Scripts/BuyAllScript.js
@@ -20,11 +20,14 @@ let tauAtPub = 0
 while (true) {
   let currency = remote(
     "theory.tick(" + tickLength + ",1.5)\n" +
-    "game.buy(theory.upgrades)\n" +
+    "theory.upgrades[1].buy(-1)\n" +
+    "theory.upgrades[2].buy(-1)\n" +
+    "theory.upgrades[3].buy(-1)\n" +
+    "theory.upgrades[0].buy(-1)\n" +
     "game.buy(theory.permanentUpgrades)\n" +
     "hasPublished = false\n" +
     "if(theory.milestonesUnused > 0){\n" +
-    "let m = [1,1,1,3,3,4,4,4,4,2,2,2,2,2]\n" +
+    "let m = [3,3,2,3,1,1,1,1]\n" +
     "theory.milestoneUpgrades[m[theory.milestonesTotal-1]-1].buy(1)}\n" +
 
     "if (theory.isPublicationAvailable) {\n" +
@@ -39,7 +42,7 @@ while (true) {
   if (remote("hasPublished") == "true") {
     log(getTimeString(totalTime) + tauAtPub)
     tickLength = 1
-  }
+    }
   if (remote("theory.milestonesUnused > 0") == "true") {
     while (remote("theory.milestonesUnused > 0") == "true") {
       System.Threading.Thread.Sleep(500)
@@ -50,3 +53,4 @@ while (true) {
   totalTime += tickLength
   tickLength = Math.min(360, tickLength*1.25)
 }
+
