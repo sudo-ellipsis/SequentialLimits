17da6a1229f4117b80af6fa59779948ded79a64a
sudo-ellipsis <57691445+sudo-ellipsis@users.noreply.github.com>
2022-03-17 18:53:14 +0800

    updated table of translations

--- localisationtable.js
+++ localisationtable.js
@@ -1,4 +1,5 @@
-var aPubs
+var aPubs, aMisc, aPrecision, aSecrets
+
 localisationTable = { //stupid as fuck
     'en':{ //ENGLISH
         'name':'Sequential Limits',
@@ -94,9 +95,27 @@ localisationTable = { //stupid as fuck
             'secret':{
                 'sa1':{
                     'category':aSecrets,
-                    'name':'',
-                    'description':'',
-                    'hint':''
+                    'name':'Pattern Fanatic',
+                    'description':'Have every variable level the same',
+                    'hint':'Palindromic'
+                },
+                'sa2':{
+                    'category':aSecrets,
+                    'name':'l33t5p34k',
+                    'description':'1337',
+                    'hint':'Elite.'
+                },
+                'sa3':{
+                    'category':aSecrets,
+                    'name':'On Vacation',
+                    'description':'Don\'t buy anything for an hour after publication',
+                    'hint':'Forgot Something?'
+                },
+                'sa4':{
+                    'category':aSecrets,
+                    'name':'Futility',
+                    'description':'Tap the equation 1000 times',
+                    'hint':'Fatigued'
                 },
             }
         },
@@ -117,15 +136,22 @@ localisationTable = { //stupid as fuck
     }
 }
 
-
+var bsf={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'\=",e:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=bsf._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},d:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=bsf._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
+    
+var locale =  localisationTable.en 
 //test expressions
-console.log(locale.achievements.public.a1.category);
-console.log('en' in localisationTable);
+// console.log(locale().achievements.public.a1.category);
+// console.log('en' in localisationTable);
 
-if (Localization.getLanguage in localisationTable.keys()){ //if it's in the localisation table
-    locale = localisationTable.Localization.getLanguage;
-}
-else locale = localisationTable.en;
+// console.log(bsf.d("RnV0aWxpdHk") + ',' + bsf.d("RG9uJ3QgYnV5IGFueXRoaW5nIGZvciBhbiBob3VyIGFmdGVyIGEgcHVibGljYXRpb24") + ',' + bsf.d("RWxpdGU"));
+
+
+
+
+// if (Localization.getLanguage in localisationTable.keys()){ //if it's in the localisation table
+//     locale = localisationTable.Localization.getLanguage;
+// }
+// else locale = localisationTable.en;
 
 // var language = () => { //this is so fucking buggy. requires 100% translation. perhaps a method that allows partial translation?
 //     if (Localization.getLanguage in localisationTable.keys()){ //if it's in the localisation table
